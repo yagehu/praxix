555bb60af4b136fd542286e50c4768b028b38668
Yage Hu <huyage@fastmail.com>
2022-03-24 00:14:45 -0700

    Init bootloader crate

---
+++ crates/bootloader/praxix.mk
@@ -0,0 +1,15 @@
+IN_CRATE_BOOTLOADER_DIR = $(IN_CRATES_DIR)/bootloader
+IN_CRATE_BOOTLOADER_SRC_DIR = $(IN_CRATE_BOOTLOADER_DIR)/src
+IN_CRATE_BOOTLOADER_SRCS = $(shell find $(IN_CRATE_BOOTLOADER_SRC_DIR) -type f -name '*.rs')
+
+OUT_CRATES_BOOTLOADER = $(OUT_CRATES_DIR)/libbootloader.rlib
+
+.PHONY: crate-bootloader
+crate-bootloader: $(OUT_CRATES_BOOTLOADER)
+
+$(OUT_CRATES_BOOTLOADER): $(IN_CRATE_BOOTLOADER_SRCS) $(OUT_RUST_CORE_LIB) $(OUT_RUST_COMPILER_BUILTINS_LIB) | $(OUT_CRATES_DIR)
+	rustc $(RUSTC_FLAGS) \
+		--crate-type rlib \
+		--crate-name bootloader \
+		-o $@ \
+		$(IN_CRATE_BOOTLOADER_SRC_DIR)/lib.rs
---
+++ crates/praxix.mk
@@ -0,0 +1,13 @@
+CRATES = \
+    bootloader
+
+CRATES_MK_FILES = $(patsubst %,$(ROOT)/crates/%/praxix.mk, $(CRATES))
+
+IN_CRATES_DIR = $(ROOT)/crates
+
+OUT_CRATES_DIR = $(OUT_TARGET_DIR)/crates
+
+include $(CRATES_MK_FILES)
+
+$(OUT_CRATES_DIR): | $(OUT_TARGET_DIR)
+	mkdir $@
